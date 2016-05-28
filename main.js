8b1d08c578ebe727e6fc4b16d2e980b23effa2e1
cha014 <c-h-F-15@kcs.org.uk>
2016-05-28 14:13:03 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
+var money = 0
+var completion = 0
+var completionPlus = 0.1
+var employNumCost = 50
 
+function getEarn() {
+  money += 1;
+  document.getElementById("moneyNum").innerHTML = money;
+}
+
+function getWork() {
+  completion += completionPlus;
+  document.getElementById("completionNum").innerHTML = completion;
+}
+
+function getEmploy() {
+  if (money > employNumCost) {
+    money = money -= employNumCost;
+    document.getElementById("moneyNum").innerHTML = money;
+    employNumCost = employNumCost += employNumCost *= 2
+    document.getElementById("employNum").innerHTML = employNumCost;
+    completionPlus = completionPlus += 1
+    document.getElementById("workExtraNum").innerHTML = completionPlus;
+    
+  }
+}
