9f200dfbe87ff3015af3d47bf9fa5f7883cc65f2
cha014 <c-h-F-15@kcs.org.uk>
2016-05-28 14:27:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -11,50 +11,6 @@ function getEarn() {
 function getWork() {
   completion += completionPlus;
   document.getElementById("completionNum").innerHTML = completion; 
-  
-  if (completion > 5 <5.3) {
-    window.alert("module completed!");
-  }
-  
-  if (completion > 10 < 10.3){
-  window.alert("boosters completed!");  
-  }
-  
-  if (completion > 20 < 20.3) {
-    window.alert("engines completed!");
-  }
-  
-  if (completion > 30 < 30.3) {
-    window.alert("fuel tanks completed!");
-  }
-  
-  if (completion > 50 < 50.3){
-    window.alert("main fusalge completed!");
-  }
-  
-  if (completion > 60 < 60.25){
-    window.alert("Oh no! your workshop has been hit by an earthquake and much of your work has been desroyed!");
-    completion *= 0.4;
-    document.getElementById("completionNum").innerHTML = completion;
-    completionPlus *= 0.4;
-     document.getElementById("workExtraNum").innerHTML = completionPlus;
-  }
-  
-  if ("completion > 75 < 75.3"){
-    window.alert("launch pad foundations completed!");
-  }
-  
- if (completion > 90 < 90.3) {
-   window.alert("launch pad completed!");
- }
- 
- if (completion > 96 < 96.3) {
-   window.alert("moving rocket onto launch - nearly there now!");
- }
- 
- if (completion < 100) {
-   window.alert("Wow! You've escaped from earth and will now survive!");
- }
 }
   
   function getEarn() {
