b72a4a719bec6abc31e0294589fa3904bd5c1593
cha014 <c-h-F-15@kcs.org.uk>
2016-05-28 14:18:34 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var money = 0
 var completion = 0
-var completionPlus = 0.1
-var employNumCost = 50
+var completionPlus = 0.01
+var learnNumCost = 100
 
 function getEarn() {
   money += 1;
@@ -13,13 +13,13 @@ function getWork() {
   document.getElementById("completionNum").innerHTML = completion;
 }
 
-function getEmploy() {
-  if (money > employNumCost) {
-    money = money -= employNumCost;
+function getLearn() {
+  if (money > learnNumCost) {
+    money = money -= learnNumCost;
     document.getElementById("moneyNum").innerHTML = money;
-    employNumCost = employNumCost += employNumCost *= 2
-    document.getElementById("employNum").innerHTML = employNumCost;
-    completionPlus = completionPlus += 1
+    learnNumCost = learnNumCost += learnNumCost *= 2
+    document.getElementById("learnNum").innerHTML = learnNumCost;
+    completionPlus = completionPlus += 0.01
     document.getElementById("workExtraNum").innerHTML = completionPlus;
     
   }
